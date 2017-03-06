37c2601f475726940baf99807ba51c52acdad72d
Daniel <daniel.j.fletcher2@gmail.com>
2017-03-06 11:39:53 -0800

    Configure store with Todos reducer

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,13 +1,23 @@
-import { RECEIVE_TODOS, RECEIVE_TODO } from './todo_actions';
+import { RECEIVE_TODOS, RECEIVE_TODO } from '../actions/todo_actions';
 
-export const todosReducer = (state = [], action) => {
+const todosReducer = (state = {}, action) => {
   Object.freeze(state);
   switch(action.type) {
-    case RECEIVE_TODOS:
-      return [...state, ...action.todos];
-    case RECEIVE_TODO:
-      return [...state, action.todo];
     default:
       return state;
   }
 };
+
+// export const todosReducer = (state = {}, action) => {
+//   Object.freeze(state);
+//   switch(action.type) {
+//     case RECEIVE_TODOS:
+//       return [...state, ...action.todos];
+//     case RECEIVE_TODO:
+//       return [...state, action.todo];
+//     default:
+//       return state;
+//   }
+// };
+
+export default todosReducer;
